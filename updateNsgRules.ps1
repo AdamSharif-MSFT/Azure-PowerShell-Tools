2f75249b8c80b3a1a13b33375cad1561ade79024
AdamSharif-MSFT <40294242+AdamSharif-MSFT@users.noreply.github.com>
2021-02-01 14:10:16 +0000

    Adding overwrite option

--- updateNsgRules.ps1
+++ updateNsgRules.ps1
@@ -1,4 +1,6 @@
-$publicIp = ((Invoke-WebRequest ifconfig.co -UserAgent curl/7.68.0).Content).Trim()
+param($overwrite=$false)
+
+$publicIp = ((Invoke-WebRequest ipv4.icanhazip.com -UserAgent curl/7.68.0).Content).Trim()
 $regex = [regex] "\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}"
 
 if ($publicIp -match $regex) {
@@ -7,10 +9,19 @@ if ($publicIp -match $regex) {
 
     $collection = Get-AzNetworkSecurityGroup
     foreach ($item in $collection) {
-        if ($item.SecurityRules.Name.Contains("Home")) {
+        if ($item.SecurityRules.Priority.contains(100)) {
             $nsgName = $item.Name
-            Write-Host "Updating NSG: $nsgName" -ForegroundColor Green
-            Set-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $item -Name "Home" -Direction Inbound -SourceAddressPrefix $publicIp -Protocol * -Access Allow  -SourcePortRange * -DestinationPortRange * -DestinationAddressPrefix * -Priority 100 | Set-AzNetworkSecurityGroup | Format-Table
+            if ($overwrite -eq $false) {
+                Write-Host "Updating NSG: $nsgName" -ForegroundColor Green
+                Set-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $item -Name "Home" -Direction Inbound -SourceAddressPrefix $publicIp -Protocol * -Access Allow  -SourcePortRange * -DestinationPortRange * -DestinationAddressPrefix * -Priority 100 | Set-AzNetworkSecurityGroup | Format-Table
+            }
+            else {
+                Write-Host "Removing existing NSG: $nsgName" -ForegroundColor Green
+                $existingRule = Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $item | Where-Object {$_.Priority -eq 100}
+                Remove-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $item -Name $existingRule.Name | Set-AzNetworkSecurityGroup | Format-Table
+                Write-Host "Adding rule to NSG: $nsgName" -ForegroundColor Green
+                Add-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $item -Name "Home"-Direction Inbound -SourceAddressPrefix $publicIp -Protocol * -Access Allow -SourcePortRange * -DestinationPortRange * -DestinationAddressPrefix * -Priority 100 | Set-AzNetworkSecurityGroup | Format-Table
+            }
         }
         else {
             $nsgName = $item.Name
